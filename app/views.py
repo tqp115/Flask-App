57c1c5c9e2750a93eb317fb384a6b84e302c0b12
tqp115 <tommypham115@gmail.com>
2023-05-02 13:57:39 -0500

    test code for site

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from flask import render_template, request, redirect, url_for, flash, jsonify
 from app import app
 
+
 @app.route('/')
+@app.route('/index')
 def index():
-    data = "hello"
-    return render_template('index.html', data=data)
+    user = {'username': 'Test'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Hello World'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Lorem ipsum'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
