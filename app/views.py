111b4bb3649be69b883934bca9c01494e0e26e18
tqp115 <tommypham115@gmail.com>
2023-05-03 15:17:51 -0500

    add sign in page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash, jsonify
 from app import app
-
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-import os
-
 DEBUG = True
-
-class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY")
