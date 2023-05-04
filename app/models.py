0d131e62923a1a1b8b3ba30fe13ec7afe0dcac95
ttaylor1819 <ttaylor@standarduserllc.com>
2023-05-04 12:20:04 -0500

    Class Post

--- app/models.py
+++ app/models.py
@@ -19,3 +19,15 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    author = db.relationship('User', backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_created}')"
