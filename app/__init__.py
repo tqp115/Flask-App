aa3e9d44d12c574f0018ef4018bfedb6a8b29967
ttaylor1819 <ttaylor@standarduserllc.com>
2023-05-04 13:15:53 -0500

    Migration Resposiory

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #initialize app
 
 app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(Config)
+db = SQLAlchemy
+migrate = Migrate(app, db)
 
 from app import views
 
--- config.py
+++ config.py
@@ -3,5 +3,12 @@ from dotenv import load_dotenv
 load_dotenv()
 DEBUG = True
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
